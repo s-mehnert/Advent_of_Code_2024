e2efb21bdb3a3758436c3ed3675fcfb973b4e6d4
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-12-08 21:01:52 +0100

    code --- Day 8 - Star 16: more grid based combinations

--- 08.py
+++ 08.py
@@ -66,17 +66,65 @@ def find_antipodes(grid: list, antennas: list) -> int:
     return antipodes
 
 
+# --- Part 2 ---
+
+# calculate the patterns further as long as they are within bounds of grid
+# count antenna positions as antinodes as well
+# do this in a new function
+
+
+def find_antipodes_v2(grid: list, antennas: list) -> int:
+    antipodes = list()
+    antenna_combos = combinations(antennas, 2)
+
+    for combo in antenna_combos:
+        a1, a2 = combo
+        a1_row, a1_col = a1
+        a2_row, a2_col = a2
+        diff_rows = a1_row-a2_row
+        diff_cols = a1_col-a2_col
+
+        antipodes.extend([a1, a2])
+
+        new_antipode_row = a1_row
+        new_antipode_col = a1_col
+
+        while new_antipode_row in range(len(grid)) and new_antipode_col in range(len(grid[0])):
+            antipodes.append((new_antipode_row, new_antipode_col))
+            new_antipode_row += diff_rows
+            new_antipode_col += diff_cols
+
+        new_antipode_row = a2_row - diff_rows
+        new_antipode_col = a2_col - diff_cols
+
+        while new_antipode_row in range(len(grid)) and new_antipode_col in range(len(grid[0])):
+            antipodes.append((new_antipode_row, new_antipode_col))
+            new_antipode_row -= diff_rows
+            new_antipode_col -= diff_cols
+
+    return list(set(antipodes))
+
+
 # calculate result
 
 antipodes = list()
+antipodes_v2 = list()
 
 for frequency in evaluate_grid(imported_data):
     new_antipodes = find_antipodes(imported_data, find_antennas(imported_data, frequency))
+    new_antipodes_v2 = find_antipodes_v2(imported_data, find_antennas(imported_data, frequency))
     for antipode in new_antipodes:
         if antipode not in antipodes:
             antipodes.append(antipode)
+    for antipode_v2 in new_antipodes_v2:
+        if antipode_v2 not in antipode_v2:
+            antipodes_v2.append(antipode_v2)
 
 result_1 = len(antipodes)
 
 print_grid(imported_data)
 print("Number of antipodes on map:", result_1)
+
+result_2 = len(list(set(antipodes_v2)))
+
+print("\nNumber of antipodes on map with new rules:", result_2)
